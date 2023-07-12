a9730dc3e4cdba160a9f63f03191e6912d6a7aa9
Kelly Ku <kellyku@Kellys-MacBook-Pro.local>
2023-07-11 21:15:51 -0700

    add user auth server side code

---
+++ server/controllers/users.js
@@ -0,0 +1,83 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const signin = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser)
+      return res
+        .status(404)
+        .json({ success: false, message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordCorrect)
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+
+    const token = jwt.sign(
+      { email: existingUser.email, id: existingUser._id },
+      process.env.JWT_ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
+
+    res
+      .status(200)
+      .json({ success: true, message: "Signedin User", data: token });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
+
+export const signup = async (req, res) => {
+  const { email, password, confirmPassword, firstName, lastName, role } =
+    req.body;
+
+  try {
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser)
+      return res
+        .status(400)
+        .json({ success: false, message: "User already exists." });
+
+    if (password !== confirmPassword)
+      return res
+        .status(400)
+        .json({ success: false, message: "Passwords don't match" });
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+    const result = await User.create({
+      email,
+      password: hashedPassword,
+      firstName,
+      lastName,
+      role,
+    });
+
+    const token = jwt.sign(
+      { email: result.email, id: result._id },
+      process.env.JWT_ACCESS_TOKEN_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    res
+      .status(200)
+      .json({ success: true, message: "Signed up", data: { result, token } });
+  } catch (e) {
+    res.status(500).json({ success: false, message: "Something went wrong" });
+  }
+};
---
+++ server/middleware/auth.js
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const auth = async (req, _, next) => {
+  try {
+    const token = req.headers.Authorization.split(" ")[1];
+    const isCustomAuth = token.length < 500;
+
+    let decodedData;
+
+    if (token && isCustomAuth) {
+      decodedData = jwt.verify(token, process.env.JWT_ACCESS_TOKEN_SECRET);
+
+      req.userId = decodedData?.id;
+    }
+
+    next();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default auth;
